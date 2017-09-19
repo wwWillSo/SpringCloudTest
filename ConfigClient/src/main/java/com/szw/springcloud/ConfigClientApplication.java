975d54c3e8bcc3698c9e73c48849285ea2b0ebb3
苏镇威 <244864220@qq.com>
2017-09-19 18:03:48 +0800

    Create README.md
    Signed-off-by: wwWillSo <244864220@qq.com>

---
+++ ConfigClient/src/main/java/com/szw/springcloud/ConfigClientApplication.java
@@ -0,0 +1,33 @@
+package com.szw.springcloud;
+	
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value("${foo}")
+    String foo;
+    
+    @Value("${foo2}")
+    String foo2;
+    
+    @RequestMapping(value = "/hi")
+    public String hi(String name){
+    	if (name.equals("foo")) {
+    		return foo ;
+    	}
+    	if (name.equals("foo2")) {
+    		return foo2 ;
+    	} else 
+    		return foo;
+    }
+}
